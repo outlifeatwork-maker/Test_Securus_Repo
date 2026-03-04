dce8c565961d1ebfcb765e37cdc4ca355caff81e
outlife <outlifeatwork@gmail.com>
2026-03-05 01:15:23 +0530

    malicious code

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,30 @@
 const express = require("express");
-const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(cors());
+const DB_PASSWORD = "super_secret_db_password";
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Backend running");
+app.post("/login", (req, res) => {
+
+  const password = req.body.password;
+
+  const userId = req.body.id;
+
+  // SQL injection
+  const query = "SELECT * FROM users WHERE id = " + userId;
+
+  console.log(query);
+
+  // JWT without expiry
+  const token = jwt.sign({ id: userId }, "MY_SECRET_KEY");
+
+  res.json({ token });
+
 });
 
-app.get("/api/data", (req, res) => {
-    res.json({
-        message: "Hello from backend"
-    });
-});
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(3001, () => {
+  console.log("Backend running");
 });
